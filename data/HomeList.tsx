e05524f38bd8a0302eb719e65d9f6d95406274f5
aiaccfin <aiaccfin@gmail.com>
2025-03-29 11:17:50 -0400

    3.7.0

--- data/HomeList.tsx
+++ data/HomeList.tsx
@@ -8,32 +8,33 @@ export const HomeList: CatInterface[] = [
     { id: "bwxy", gh: "aidres8", title: "Wang Xinyao 🔥 王馨瑶" },
     { id: "bcmy", gh: "aidres8", title: "Chu Mengyao 💋 初梦瑶" },
 
-    // { id: "bnabi", gh: "aidres8", title: "Yeon 娜比 🌸", },
-    // { id: "absyz", gh: "ai8ai", title: "Son Yoon Joo 💖 손윤주" },
+    { id: "bnabi", gh: "aidres8", title: "Yeon 娜比 🌸", },
+    { id: "absyz", gh: "ai8ai", title: "Son Yoon Joo 💖 손윤주" },
 
-    // { id: "abkele", gh: "ai8ai", title: "Kele Vicky 🍑" },
-    // { id: "abanran", gh: "ai8ai", title: "Anran ✨ 安然" },
+    { id: "abkele", gh: "ai8ai", title: "Kele Vicky 🍑" },
+    { id: "abanran", gh: "ai8ai", title: "Anran ✨ 安然" },
 
-    // { id: "abzyx", gh: "ai8ai", title: "Carol 周妍希 🔥" },
-    // { id: "abxlz", gh: "ai8ai", title: "Xu Lizhi 🍑 徐莉芝" },
 
-    // { id: "abccc", gh: "ai8ai", title: "Cheng Chengcheng 💃 程程程" },
-    // { id: "ablxl", gh: "ai8ai", title: "Lin Xinglan 🌹 林星阑" },
+    { id: "abccc", gh: "ai8ai", title: "Chengcheng 💃 程程程" },
+    { id: "ablxl", gh: "ai8ai", title: "Lin Xinglan 🌹 林星阑" },
 
-    // { id: "absuren", gh: "ai8ai", title: "Girl Next Door 👗 素人しろうと" },
-    // { id: "abcel", gh: "ai8ai", title: "Celebrities 🌟" },
+    { id: "abzyx", gh: "ai8ai", title: "Carol 周妍希 🔥" },
+    { id: "abxlz", gh: "ai8ai", title: "Xu Lizhi 🍑 徐莉芝" },
 
-    // { id: "abspecialtaste", gh: "ai8ai", title: "Special Taste 😈" },
-    // { id: "absketch", gh: "ai8ai", title: "Sketch & Hand-drawing ✏️" },
+    { id: "absuren", gh: "ai8ai", title: "Next Door👗素人しろうと" },
+    { id: "abcel",   gh: "ai8ai", title: "Celebrities 🌟" }, 
 
-    // { id: "abmini", gh: "ai8ai", title: "Modern Minimalist 🛋️" },
-    // { id: "abss", gh: "ai8ai", title: "Signature Styles 🔥" },
+    { id: "abspecialtaste", gh: "ai8ai", title: "Special Taste 😈" },
+    { id: "absketch", gh: "ai8ai", title: "Sketch & Hand-drawing ✏️" },
 
-    // { id: "abplaces", gh: "ai8ai", title: "Places 🌆" },
-    // { id: "abholiday", gh: "ai8ai", title: "Holidays 🎉" },
+    { id: "abmini", gh: "ai8ai", title: "Modern Minimalist 🛋️" },
+    { id: "abss", gh: "ai8ai", title: "Signature Styles 🔥" },
 
-    // { id: "igif", gh: "ai8ai", title: "GIF MP4 🎞️" },
-    // { id: "abpast", gh: "ai8ai", title: "Past Year Collection 📸" },
+    { id: "abplaces", gh: "ai8ai", title: "Places 🌆" },
+    { id: "abholiday", gh: "ai8ai", title: "Holidays 🎉" },
+
+    { id: "bgif", gh: "aidres8", title: "GIF MP4 🎞️" },
+    { id: "abpast", gh: "ai8ai", title: "Past Year Collection 📸" },
 ].map(item => ({...item,
     cover: genJpg1(`${item.gh}`, `${item.id}/main/cat`)[0]   // cat is folder name.
 }));
